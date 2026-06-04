28df019918b0e4ca9f95a5fba36a9936151d6b29
haiseskibidi <akopalexx@mail.ru>
2026-06-04 22:41:31 +1000

    feat(physics): add RAMP shape geometry to VoxelShape and implement value equality

--- src/main/java/com/za/zenith/world/physics/VoxelShape.java
+++ src/main/java/com/za/zenith/world/physics/VoxelShape.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VoxelShape {
+    public enum ShapeGeometry {
+        CUBIC,
+        RAMP
+    }
+
     private final List<AABB> boxes;
+    private ShapeGeometry geometry = ShapeGeometry.CUBIC;
+    private byte metadata = 0;
     
     public VoxelShape() {
         this.boxes = new ArrayList<>();
@@ -46,6 +53,37 @@ public class VoxelShape {
     public static final VoxelShape SLAB_SOUTH = new VoxelShape(new AABB(0, 0, 0.5f, 1, 1, 1));
     public static final VoxelShape SLAB_WEST = new VoxelShape(new AABB(0, 0, 0, 0.5f, 1, 1));
     public static final VoxelShape SLAB_EAST = new VoxelShape(new AABB(0.5f, 0, 0, 1, 1, 1));
+
+    public ShapeGeometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(ShapeGeometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public byte getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(byte metadata) {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VoxelShape)) return false;
+        VoxelShape that = (VoxelShape) o;
+        return metadata == that.metadata &&
+               geometry == that.geometry &&
+               java.util.Objects.equals(boxes, that.boxes);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(boxes, geometry, metadata);
+    }
 }
 
 
